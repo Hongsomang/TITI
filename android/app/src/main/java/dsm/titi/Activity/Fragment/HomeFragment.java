e5f71d617e870b363abc6aca68ba45ce988c0512
Hongsomang <ghdthakd12@naver.com>
2018-04-19 23:44:54 +0900

    .

--- android/app/src/main/java/dsm/titi/Activity/Fragment/HomeFragment.java
+++ android/app/src/main/java/dsm/titi/Activity/Fragment/HomeFragment.java
@@ -13,6 +13,14 @@ import android.widget.FrameLayout;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationViewPager;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import dsm.titi.R;
 
@@ -20,11 +28,11 @@ import dsm.titi.R;
  * Created by ghdth on 2018-04-17.
  */
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
     private FrameLayout fragmentContainer;
     private FrameLayout fragmentContainer2;
     private FrameLayout fragmentContainer3;
-
+    private MapView mapView;
     private AHBottomNavigation bottomNavigation;
     private AHBottomNavigationViewPager viewPager;
 
@@ -45,12 +53,20 @@ public class HomeFragment extends Fragment {
             return view;
         }
         if(getArguments().getInt("index",0)==1){
+            view=inflater.inflate(R.layout.fragment_map,container,false);
+            mapView=(MapView)view.findViewById(R.id.map);
+            mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(this);
+            //initmap(view);
+
             return view;
         }
         if(getArguments().getInt("index",0)==2){
         }
         return view;
     }
+
+
     public void willBeHidden(){
         if(fragmentContainer!=null){
             Animation fadOut= AnimationUtils.loadAnimation(getActivity(), R.anim.fade_out);
@@ -72,6 +88,31 @@ public class HomeFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        mapView.onResume();
         Log.d("dfdf","dfdfds");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(this.getActivity());
+        CameraUpdate cameraUpdate= CameraUpdateFactory.newLatLngZoom(new LatLng(35.141233, 126.925594), 14);
+
+        googleMap.animateCamera(cameraUpdate);
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(35.141233, 126.925594))
+                .title("루프리코리아"));
     }
 }
